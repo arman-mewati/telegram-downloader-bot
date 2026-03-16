80b52a9befe8be6bbbaae97e972fb67eda5f94c5
arman-mewati <armanofficial489@gmail.com>
2026-03-16 20:51:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 import yt_dlp
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
 
 TOKEN = "8628787355:AAFClhBFZyfu8XkRNFiXxeVSjCJgoqoHm9o"
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Send Instagram or Facebook video link.")
+
 async def downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
+
+    if "http" not in url:
+        await update.message.reply_text("Please send a valid video link.")
+        return
 
     ydl_opts = {
         'format': 'best',
@@ -19,6 +26,7 @@ async def downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT, downloader))
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, downloader))
 
 app.run_polling()
